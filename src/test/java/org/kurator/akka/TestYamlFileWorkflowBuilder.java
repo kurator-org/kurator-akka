da017105d52b9c97f369c481c66e36b3ec42fbf5
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-10-27 11:31:11 -0700

    Added declaration of yaml types to simplify yaml declarations of akka workflows.

--- src/test/java/org/kurator/akka/TestYamlFileWorkflowBuilder.java
+++ src/test/java/org/kurator/akka/TestYamlFileWorkflowBuilder.java
@@ -3,25 +3,25 @@ package org.kurator.akka;
 import junit.framework.TestCase;
 
 import org.kurator.akka.WorkflowBuilder;
-import org.kurator.akka.YamlStringWorkflowBuilder;
 import org.kurator.akka.messages.EndOfStream;
 
 import akka.actor.ActorRef;
 
 public class TestYamlFileWorkflowBuilder extends TestCase {
     
-    public static final String EOL = System.getProperty("line.separator");
+    static final String EOL = System.getProperty("line.separator");
+    static final String RESOURCE_PATH = "classpath:/org/kurator/akka/test/TestYamlFileWorkflowBuilder/";
     
     public void testEmptyWorkflow() throws Exception {
         
-        WorkflowBuilder builder = new YamlFileWorkflowBuilder("classpath:/org/kurator/akka/test/TestYamlFileWorkflowBuilder/empty_workflow.yaml");
+        WorkflowBuilder builder = new YamlFileWorkflowBuilder(RESOURCE_PATH + "empty_workflow.yaml");
         builder.build();
         ActorRef workflowRef = builder.getWorkflowRef();
         assertNotNull(workflowRef);
 }
 
     public void testOneActorWorkflow() throws Exception {
-        WorkflowBuilder builder = new YamlFileWorkflowBuilder("classpath:/org/kurator/akka/test/TestYamlFileWorkflowBuilder/one_actor_workflow.yaml");
+        WorkflowBuilder builder = new YamlFileWorkflowBuilder(RESOURCE_PATH + "one_actor_workflow.yaml");
         builder.build();
         builder.startWorkflow();
         builder.tellWorkflow(1);
@@ -30,7 +30,7 @@ public class TestYamlFileWorkflowBuilder extends TestCase {
     }
     
     public void testTwoActorWorkflow() throws Exception {
-        WorkflowBuilder builder = new YamlFileWorkflowBuilder("classpath:/org/kurator/akka/test/TestYamlFileWorkflowBuilder/two_actor_workflow.yaml");
+        WorkflowBuilder builder = new YamlFileWorkflowBuilder(RESOURCE_PATH + "two_actor_workflow.yaml");
         builder.build();
         builder.startWorkflow();
         builder.tellWorkflow(1);
@@ -40,7 +40,23 @@ public class TestYamlFileWorkflowBuilder extends TestCase {
     }
 
     public void testThreeActorWorkflow() throws Exception {        
-        WorkflowBuilder builder = new YamlFileWorkflowBuilder("classpath:/org/kurator/akka/test/TestYamlFileWorkflowBuilder/three_actor_workflow.yaml");
+        WorkflowBuilder builder = new YamlFileWorkflowBuilder(RESOURCE_PATH + "three_actor_workflow.yaml");
+        builder.build();
+        builder.startWorkflow();
+        builder.tellWorkflow(1);
+        builder.tellWorkflow(2);
+        builder.tellWorkflow(3);
+        builder.tellWorkflow(4);
+        builder.tellWorkflow(5);
+        builder.tellWorkflow(6);
+        builder.tellWorkflow(4);
+        builder.tellWorkflow(3);
+        builder.tellWorkflow(new EndOfStream());
+        builder.awaitWorkflow();
+    }
+    
+    public void testThreeActorWorkflowSimplified() throws Exception {        
+        WorkflowBuilder builder = new YamlFileWorkflowBuilder(RESOURCE_PATH + "three_actor_workflow_simplified.yaml");
         builder.build();
         builder.startWorkflow();
         builder.tellWorkflow(1);
