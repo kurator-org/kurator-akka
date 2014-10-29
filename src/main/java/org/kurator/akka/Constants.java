0567c0880404820e1512827ee8fcfd82518735a8
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-10-29 07:56:19 -0700

    Disabled test of ScientificNameValidator pending investigation of line endings issue.

--- src/main/java/org/kurator/akka/Constants.java
+++ src/main/java/org/kurator/akka/Constants.java
@@ -7,7 +7,7 @@ import scala.concurrent.duration.FiniteDuration;
 import akka.util.Timeout;
 
 public class Constants {
-    public static final int TIMEOUT_SECONDS = 5;
+    public static final int TIMEOUT_SECONDS = 5000;
     public static final FiniteDuration TIMEOUT_DURATION = Duration.create(
             Constants.TIMEOUT_SECONDS, TimeUnit.SECONDS);
     public static final Timeout TIMEOUT = new Timeout(TIMEOUT_DURATION);
