e06c00213a459937e1c47f7aed5e9b709ac8a9e8
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-11-28 10:04:04 -0800

    Removed unresolved dependency on org.junit.

--- src/test/java/org/kurator/akka/TestKuratorAkka.java
+++ src/test/java/org/kurator/akka/TestKuratorAkka.java
@@ -4,8 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
-import org.junit.Test;
-
 import junit.framework.TestCase;
 
 public class TestKuratorAkka extends TestCase {
@@ -33,7 +31,6 @@ public class TestKuratorAkka extends TestCase {
         errPrintStream = new PrintStream(errOutputBuffer);
     }
     
-    @Test
     public void testKuratorAkka_NoArgs() throws Exception {
         String[] args = {};
         KuratorAkka.runWorkflowForArgs(args, null, errPrintStream);
@@ -43,7 +40,6 @@ public class TestKuratorAkka extends TestCase {
             errOutputBuffer.toString());
     }
 
-    @Test
     public void testKuratorAkka_HelpOption() throws Exception {
         String[] args = {"--help"};
         KuratorAkka.runWorkflowForArgs(args, null, errPrintStream);
@@ -52,7 +48,6 @@ public class TestKuratorAkka extends TestCase {
             errOutputBuffer.toString());
     }
 
-    @Test
     public void testKuratorAkka_HelpOption_Abbreviation() throws Exception {
         String[] args = {"-h"};
         KuratorAkka.runWorkflowForArgs(args, null, errPrintStream);
@@ -61,7 +56,6 @@ public class TestKuratorAkka extends TestCase {
             errOutputBuffer.toString());
     }
     
-    @Test
     public void testKuratorAkka_FileOption_NoArgument() throws Exception {
         String[] args = {"-f"};
         KuratorAkka.runWorkflowForArgs(args, null, errPrintStream);
@@ -72,7 +66,6 @@ public class TestKuratorAkka extends TestCase {
             errOutputBuffer.toString());
     }
     
-    @Test
     public void testKuratorAkka_FileOption_ClasspathScheme_HammingWorkflow() throws Exception {
         String[] args = {"-f", "classpath:/org/kurator/akka/samples/hamming.yaml"};
         KuratorAkka.runWorkflowForArgs(args, outPrintStream, errPrintStream);
@@ -99,7 +92,6 @@ public class TestKuratorAkka extends TestCase {
     }
 
     
-    @Test
     public void testKuratorAkka_FileOption_FileScheme_HammingWorkflow() throws Exception {
         String[] args = {"-f", "file:src/main/resources/org/kurator/akka/samples/hamming.yaml"};
         KuratorAkka.runWorkflowForArgs(args, outPrintStream, errPrintStream);
@@ -125,7 +117,6 @@ public class TestKuratorAkka extends TestCase {
             stdOutputBuffer.toString());
     }
  
-    @Test
     public void testKuratorAkka_FileOption_ImplicitFileScheme_HammingWorkflow() throws Exception {
         String[] args = {"-f", "src/main/resources/org/kurator/akka/samples/hamming.yaml"};
         KuratorAkka.runWorkflowForArgs(args, outPrintStream, errPrintStream);
@@ -151,8 +142,6 @@ public class TestKuratorAkka extends TestCase {
             stdOutputBuffer.toString());
     }
     
-    
-    @Test
     public void testKuratorAkka_InputOption_HammingWorkflow() throws Exception {
         String[] args = {"-f", "classpath:/org/kurator/akka/samples/hamming.yaml", "--input", "max=5"};
         KuratorAkka.runWorkflowForArgs(args, outPrintStream, errPrintStream);
@@ -165,7 +154,6 @@ public class TestKuratorAkka extends TestCase {
             stdOutputBuffer.toString());
     }
 
-    @Test
     public void testKuratorAkka_InputOption_Abbreviated_HammingWorkflow() throws Exception {
         String[] args = {"-f", "classpath:/org/kurator/akka/samples/hamming.yaml", "-i", "max=5"};
         KuratorAkka.runWorkflowForArgs(args, outPrintStream, errPrintStream);
