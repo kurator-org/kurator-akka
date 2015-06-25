1e533aec60090fec1ada4d14e12ff0e373938b1a
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-06-24 19:23:40 -0700

    Jython function actors can now be triggered.

--- src/main/java/org/kurator/akka/actors/JythonFunctionActor.java
+++ src/main/java/org/kurator/akka/actors/JythonFunctionActor.java
@@ -4,16 +4,25 @@ public class JythonFunctionActor extends JythonActor {
 
     public String function = "function";
     
-    private static final String wrapperFormat = 
-            "def wrapper():"                                + EOL +
+    private static final String callWrapperFormat = 
+            "def call_function():"                          + EOL +
             "  global " + inputName                         + EOL +
             "  global " + outputName                        + EOL +
             "  " + outputName + " = %s(" + inputName + ")"  + EOL;
-    
+
+    private static final String triggerWrapperFormat = 
+            "def trigger_function():"                       + EOL +
+            "  global " + outputName                        + EOL +
+            "  " + outputName + " = %s()"                   + EOL;
+
     @Override
     protected void handleInitialize() {
         super.handleInitialize();
-        interpreter.exec(String.format(wrapperFormat, function));
+        if (this.needsTrigger) {
+            interpreter.exec(String.format(triggerWrapperFormat, function));
+        } else {
+            interpreter.exec(String.format(callWrapperFormat, function));
+        }
     }
     
     @Override
@@ -22,6 +31,19 @@ public class JythonFunctionActor extends JythonActor {
     }
 
     @Override
+    protected void handleTrigger() throws Exception {
+        Object output = triggerJythonFunction();
+        handleOutput(output);
+        endStreamAndStop();
+    }
+
+    private void handleOutput(Object output) {
+        if (output != null || broadcastNulls) {
+            broadcast(output);
+        }
+    }
+    
+    @Override
     public void handleData(Object value) {  
 
         if (outputTypeIsInputType) {
@@ -29,11 +51,22 @@ public class JythonFunctionActor extends JythonActor {
         }
 
         Object output = callJythonFunction(value);
-        if (output != null || broadcastNulls) {
-            broadcast(output);
-        }
+
+        handleOutput(output);
     }
 
+    protected Object triggerJythonFunction() {
+        
+        // reset output variable to null
+        interpreter.set(outputName, none);
+        
+        // call the python function
+        interpreter.eval("trigger_function()");
+        
+        // return the function output
+        return interpreter.get(outputName, outputType);
+    }
+    
     protected Object callJythonFunction(Object input) {
         
         // reset output variable to null
@@ -43,10 +76,11 @@ public class JythonFunctionActor extends JythonActor {
         interpreter.set(inputName, input);
         
         // call the python function
-        interpreter.eval("wrapper()");
+        interpreter.eval("call_function()");
         
         // return the function output
         return interpreter.get(outputName, outputType);
     }
-    
+
+
 }
