7240b2b394173176ee9aab79647b1dec9373fcb7
Paul J. Morris <mole@morris.net>
2016-02-16 13:23:26 -0500

    ISSUE: PURPOSE: Minor fixes and generalization in Kurator-Akka. DESCRIPTION: Fixing minor bug in csv file reader when file isn't found.  Generalizing Multiplier to more number types.  Expanding and documenting tests for blank values in Util.

--- src/main/java/org/kurator/akka/data/Util.java
+++ src/main/java/org/kurator/akka/data/Util.java
@@ -2,11 +2,32 @@ package org.kurator.akka.data;
 
 public class Util {
 	
-	public static boolean isBlank(String s) {
+	/**
+	 * Test to see if a string has non whitespace content.
+	 * 
+	 * @param s the string to test for content
+	 * @return true if the string has zero length or contains only whitespace.
+	 * @throws NullPointerException it s is null.
+	 */
+	public static boolean isBlank(String s) throws NullPointerException {
 		return s.trim().isEmpty();
 	}
 	
+	/**
+	 * Test to see if a string has content.  
+	 * 
+	 * @param s the string to test for content.
+	 * @return true if the string is not null and not blank, 
+	 *    false if s is null or the string has zero length or 
+	 *    if the string contains only whitespace.
+	 */
 	public static boolean hasContent(String s) {
-		return s != null && ! isBlank(s);
+		boolean result = true;
+		try { 
+			result = !isBlank(s);
+		} catch (NullPointerException e){ 
+			result = false;
+		}
+		return result;
 	}
 }
