e1916940df0e56ba16581e880ca91434afac2f17
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-03-20 17:07:24 -0700

    BroadcastEventCountChecker now keeps message count for each sender.

--- src/main/java/org/kurator/akka/metadata/MessageSendEvent.java
+++ src/main/java/org/kurator/akka/metadata/MessageSendEvent.java
@@ -1,14 +1,12 @@
 package org.kurator.akka.metadata;
 
-import akka.actor.Actor;
-
 public class MessageSendEvent implements MetadataItem {
     
-    public final Actor actor;
+    public final int actorId;
     public final int ordinal;
     
-    public MessageSendEvent(Actor actor, int ordinal) {
-        this.actor = actor;
+    public MessageSendEvent(int actorId, int ordinal) {
+        this.actorId = actorId;
         this.ordinal = ordinal;
     }
 }
