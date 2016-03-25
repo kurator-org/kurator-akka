f14ec3ff59c4fc66c4cb3c7784c619ae40f6de2a
Paul J. Morris <mole@morris.net>
2016-03-25 07:31:05 -0400

    ISSUE: PURPOSE: Allow geo_validator.yaml workflow to run offline without exceptions. DESCRIPTION: Added checks for empty report objects in DQReportWriter.

--- src/main/java/org/kurator/akka/actors/DQReportWriter.java
+++ src/main/java/org/kurator/akka/actors/DQReportWriter.java
@@ -46,86 +46,89 @@ public class DQReportWriter extends KuratorActor {
           file.write("]");
           file.flush();
           file.close();
-          System.out.print("\n DQ Report wrote in: "+filePath+" \n ");
+          System.out.print("\n DQ Report written to: "+filePath+" \n ");
       }
   }
   public void jsonDQReport(){
-    Map<String,String> dr = new HashMap();
-    if(report.getMeasures().size()>0)
-      dr = report.getMeasures().get(0).getDataResource();
-    else if(report.getValidations().size()>0)
-      dr = report.getMeasures().get(0).getDataResource();
-    else if(report.getImprovements().size()>0)
-      dr = report.getImprovements().get(0).getDataResource();
-    else dr = null;
-
-    JSONObject dataResource = new JSONObject();
-    if(dr!=null){
-      Iterator it = dr.entrySet().iterator();
-      while (it.hasNext()) {
-          Map.Entry pair = (Map.Entry)it.next();
-          dataResource.put(pair.getKey(),pair.getValue());
-          it.remove(); // avoids a ConcurrentModificationException
-      }
+    if (report!=null) { 
+        Map<String,String> dr = new HashMap();
+        if(report.getMeasures().size()>0)
+          dr = report.getMeasures().get(0).getDataResource();
+        else if(report.getValidations().size()>0)
+          dr = report.getMeasures().get(0).getDataResource();
+        else if(report.getImprovements().size()>0)
+          dr = report.getImprovements().get(0).getDataResource();
+        else dr = null;
+    
+        JSONObject dataResource = new JSONObject();
+        if(dr!=null){
+          Iterator it = dr.entrySet().iterator();
+          while (it.hasNext()) {
+              Map.Entry pair = (Map.Entry)it.next();
+              dataResource.put(pair.getKey(),pair.getValue());
+              it.remove(); // avoids a ConcurrentModificationException
+          }
+        }
+        JSONArray measures = new JSONArray();
+        if(report.getMeasures().size()>0){
+          for(Measure item : report.getMeasures()){
+            JSONObject obj = new JSONObject();
+          	obj.put("dimension", item.getDimension());
+          	obj.put("specification", item.getSpecification());
+            obj.put("mechanism", item.getMechanism());
+            obj.put("result", item.getResult());
+            measures.add(obj);
+          }
+        }
+        JSONArray validations = new JSONArray();
+        if(report.getValidations().size()>0){
+          for(Validation item : report.getValidations()){
+            JSONObject obj = new JSONObject();
+          	obj.put("criterion", item.getCriterion());
+          	obj.put("specification", item.getSpecification());
+            obj.put("mechanism", item.getMechanism());
+            obj.put("result", item.getResult());
+            validations.add(obj);
+          }
+        }
+        JSONArray improvements = new JSONArray();
+        if(report.getImprovements().size()>0){
+          for(Improvement item : report.getImprovements()){
+            JSONObject obj = new JSONObject();
+          	obj.put("enhancement", item.getEnhancement());
+          	obj.put("specification", item.getSpecification());
+            obj.put("mechanism", item.getMechanism());
+            obj.put("result", item.getResult());
+            improvements.add(obj);
+          }
+        }
+        JSONObject dqReport = new JSONObject();
+        dqReport.put("dataResource", dataResource);
+        dqReport.put("measures", measures);
+        dqReport.put("validations", validations);
+        dqReport.put("improvements", improvements);
+    
+        JSONObject root = new JSONObject();
+        root.put("DQReport", dqReport);
+      	try {
+      		//file = new FileWriter(filePath);
+      		file.write(root.toJSONString()+",");
+    
+      	} catch (IOException e) {
+      		e.printStackTrace();
+      	}
     }
-    JSONArray measures = new JSONArray();
-    if(report.getMeasures().size()>0){
-      for(Measure item : report.getMeasures()){
-        JSONObject obj = new JSONObject();
-      	obj.put("dimension", item.getDimension());
-      	obj.put("specification", item.getSpecification());
-        obj.put("mechanism", item.getMechanism());
-        obj.put("result", item.getResult());
-        measures.add(obj);
-      }
-    }
-    JSONArray validations = new JSONArray();
-    if(report.getValidations().size()>0){
-      for(Validation item : report.getValidations()){
-        JSONObject obj = new JSONObject();
-      	obj.put("criterion", item.getCriterion());
-      	obj.put("specification", item.getSpecification());
-        obj.put("mechanism", item.getMechanism());
-        obj.put("result", item.getResult());
-        validations.add(obj);
-      }
-    }
-    JSONArray improvements = new JSONArray();
-    if(report.getImprovements().size()>0){
-      for(Improvement item : report.getImprovements()){
-        JSONObject obj = new JSONObject();
-      	obj.put("enhancement", item.getEnhancement());
-      	obj.put("specification", item.getSpecification());
-        obj.put("mechanism", item.getMechanism());
-        obj.put("result", item.getResult());
-        improvements.add(obj);
-      }
-    }
-    JSONObject dqReport = new JSONObject();
-    dqReport.put("dataResource", dataResource);
-    dqReport.put("measures", measures);
-    dqReport.put("validations", validations);
-    dqReport.put("improvements", improvements);
-
-    JSONObject root = new JSONObject();
-    root.put("DQReport", dqReport);
-  	try {
-  		//file = new FileWriter(filePath);
-  		file.write(root.toJSONString()+",");
-
-  	} catch (IOException e) {
-  		e.printStackTrace();
-  	}
   }
   public void consoleDQReport(){
-    if(report.getMeasures().size()>0){
-      System.out.println("\033[0;1m ================= DQ REPORT ================= \033[0;0m \n ");
+    System.out.println("\033[0;1m ================= DQ REPORT ================= \033[0;0m \n ");
+    if(report!=null) {  
       System.out.println("\t ::: DATA ::: \n");
       System.out.println("\033[0;1m ID: \033[0;0m "+report.getMeasures().get(0).getDataResource().get("Id"));
       System.out.println("\033[0;1m Latitude: \033[0;0m "+report.getMeasures().get(0).getDataResource().get("decimalLatitude"));
       System.out.println("\033[0;1m Longitude: \033[0;0m"+report.getMeasures().get(0).getDataResource().get("decimalLongitude"));
       System.out.println("\033[0;1m Scientific Name: \033[0;0m"+report.getMeasures().get(0).getDataResource().get("scientificName"));
       System.out.println("\033[0;1m Country Code: \033[0;0m "+report.getMeasures().get(0).getDataResource().get("countryCode"));
+      if (report.getMeasures().size()>0){
       System.out.println("\n \t ::: MEASURES ::: \n");
       for(Measure item : report.getMeasures()){
         if(item.getResult()!=null){
@@ -134,6 +137,7 @@ public class DQReportWriter extends KuratorActor {
           System.out.println("\t \033[0;1m Mechanism: \033[0;0m"+item.getMechanism());
           System.out.println("\n");
         }
+      }
       }
       if(report.getValidations().size()>0){
         System.out.println("\t ::: VALIDATIONS ::: \n");
