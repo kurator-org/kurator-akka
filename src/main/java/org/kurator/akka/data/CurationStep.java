b7e0b6da33bc84b72a23d16622acb595830259cb
lowery <lowerydb@gmail.com>
2016-06-14 14:37:08 -0400

    Minor chnage to CurationStep. Adding missing getCurationComments method.

--- src/main/java/org/kurator/akka/data/CurationStep.java
+++ src/main/java/org/kurator/akka/data/CurationStep.java
@@ -90,6 +90,10 @@ public class CurationStep {
 	public void addCurationComment(String curationComment) { 
 		curationComments.add(curationComment);
 	}
+
+	public List<String> getCurationComments() {
+		return new ArrayList<String>(curationComments);
+	}
 	
 	public void addCurationState(String curationState) { 
 		curationStates.add(curationState);
