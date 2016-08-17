cd67617f5906da0783cecd66433bb91d2b0c2b9b
David Lowery <lowery@cs.umb.edu>
2016-08-16 22:38:56 -0400

    Moving id and type to base class

--- src/main/java/org/kurator/yaml/ActorConfig.java
+++ src/main/java/org/kurator/yaml/ActorConfig.java
@@ -8,25 +8,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName(value = "PythonActor")
 public class ActorConfig extends YamlComponent {
 
-    private String id;
-    private String type;
     private ActorProperties properties;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public ActorProperties getProperties() {
         return properties;
--- src/main/java/org/kurator/yaml/YamlComponent.java
+++ src/main/java/org/kurator/yaml/YamlComponent.java
@@ -16,4 +16,22 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 })
 public abstract class YamlComponent {
 
+    private String id;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
