17ebbc75d131edf47a6bc278eafc876468d1e02a
lowery <lowery@cs.umb.edu>
2016-11-29 14:40:39 -0500

    Fixing classpath issues when running python/java workflow from command-line

--- src/main/java/org/kurator/akka/PythonActor.java
+++ src/main/java/org/kurator/akka/PythonActor.java
@@ -227,24 +227,15 @@ public class PythonActor extends KuratorActor {
         String jythonPath = null;
         String jythonHome = null;
 
-        if (System.getenv("JYTHON_PATH") != null && !System.getenv("JYTHON_PATH").isEmpty()) {
-            jythonPath = System.getenv("JYTHON_PATH");
-        } else if (configuration.containsKey("jython_path") && configuration.get("jython_path") != null){
+        if (configuration.containsKey("jython_path") && configuration.get("jython_path") != null){
             jythonPath = configuration.get("jython_path").toString();
-
-        }
-
-        if (System.getenv("JYTHON_HOME") != null && !System.getenv("JYTHON_HOME").isEmpty()) {
-            jythonHome = System.getenv("JYTHON_HOME");
-        } else if (configuration.containsKey("jython_home") && configuration.get("jython_home") != null) {
-            jythonHome = configuration.get("jython_home").toString();
-        }
-
-        if (jythonPath != null)
             interpreter.exec("sys.path.append(\"" + jythonPath + "\")");
+        }
 
-        if (jythonHome != null)
+        if (configuration.containsKey("jython_home") && configuration.get("jython_home") != null) {
+            jythonHome = configuration.get("jython_home").toString();
             interpreter.exec("sys.path.append(\"" + jythonHome + "/Lib/site-packages\")");
+        }
     }
     
     private Boolean isGlobalFunction(String f) {
